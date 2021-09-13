d145d400a271451cd8bd5a9a1d4e947fc7dfcfd1
fly <854221237@qq.com>
2021-09-13 18:06:55 +0800

    ADD: sorting algorithm

---
+++ Sorting-Algorithm/test.py
@@ -0,0 +1,13 @@
+def insertionSort(arr):
+    for i in range(len(arr)):
+        preIndex = i-1
+        current = arr[i]
+        while preIndex >= 0 and arr[preIndex] > current:
+            arr[preIndex+1] = arr[preIndex]
+            preIndex -= 1
+        arr[preIndex+1] = current
+    return arr
+
+arr = [3,1,2,5,4]
+sort_arr = insertionSort(arr)
+print(sort_arr)
